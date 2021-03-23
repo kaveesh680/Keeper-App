0fbdd00399b6250fb7ffceaade8efe3b914da325
kaveesh680 <63943539+kaveesh680@users.noreply.github.com>
2021-03-23 09:07:01 +0530

    Finishing commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
 import Note from './Note.jsx';
-import Notes from '../notes.js';
+import CreateArea from './CreateArea.jsx';
 
 
 
 function App() {
+
+    const [notes, setNotes] = useState([]);
+
+    function handleClick(note) {
+        setNotes((prevValue) => [...prevValue, note])
+    }
+
+    function deleteNote(id) {
+        setNotes((prevNotes) => {
+            return prevNotes.filter((note, index) => {
+                return index !== id;
+            });
+        })
+    }
+
     return (
         <div>
             <Header />
-            {Notes.map(note => <Note
-                key={note.key}
+            <CreateArea onAdd={handleClick} />
+            {notes.map((note, index) => (<Note
+                key={index}
+                id={index}
                 title={note.title}
                 content={note.content}
-            />
-            )}
+                onChecked={deleteNote}
+            />))}
+
+
             <Footer />
         </div>
     );
---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+
+function CreateArea(props) {
+
+    const [note, setNote] = useState({ title: "", content: "" });
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+
+        setNote(prevValue => ({ ...prevValue, [name]: value }))
+    }
+
+
+
+
+    return (<div className="form">
+
+        <input onChange={handleChange} name="title" placeholder="Title" value={note.title} />
+        <textarea onChange={handleChange} name="content" placeholder="Take a note..." rows="3" value={note.content} />
+        <button onClick={() => { props.onAdd(note); setNote({ title: "", content: "" }) }}>Add</button>
+
+    </div>);
+}
+
+export default CreateArea;
